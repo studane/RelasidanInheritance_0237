ebe8ca5b4315e5778ec72bac927ecf9ed92702f3
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-11 18:46:57 +0700

    deklarasi dan pemberian nilai object pointer varManusia

--- komposisi.cpp
+++ komposisi.cpp
@@ -5,3 +5,9 @@ using namespace std;
 //menyertakan file header jantung.h dan manusia.h
 #include "jantung.h"
 #include "manusia.h"
+
+int main()
+{
+    //deklarasi dan pemberian nilai object pointer varManusia
+    Manusia* varManusia = new Manusia("jono");
+  
