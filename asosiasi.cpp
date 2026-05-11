b48a01d5435eeee5815549a0347c74db755ab8b8
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-11 18:37:23 +0700

    deklarasi dan definisi prosedur

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -43,4 +43,37 @@ public:
         cout << "Dokter \"" << nama << "\" tidak ada\n";
     }
 
-    
+    //deklarasi prosedur tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
+};
+
+//pendefinisian prosedur - prosedur dari class dokter dan class pasien diluar class
+void pasien::tambahDoker(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+};
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDoker(this);
+}
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari Dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
