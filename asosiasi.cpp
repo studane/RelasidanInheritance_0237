54a0c592c924e18fef74cf1204be0390f3824576
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-11 18:36:13 +0700

    membuat class dan deklarasi variabel

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -26,3 +26,12 @@ public:
     void cetakDokter();
 
 };
+
+//pembuatan class dokter
+class dokter {
+public:
+    //deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    
