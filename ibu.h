d5d75173627b041804eb0f5a39b2d0cf0d8a557d
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-11 18:24:54 +0700

    membuat constructor dan destructor

--- ibu.h
+++ ibu.h
@@ -10,4 +10,12 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
-   
+    //membuat constructor dan destructor pada class ibu
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    };
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    };
+
