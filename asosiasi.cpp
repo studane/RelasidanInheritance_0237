548fc83d54d6ac45a20594d940526ee1f7db8de7
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-11 18:35:02 +0700

    pembuatan constructor dan destructor dari class pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -12,4 +12,13 @@ public:
     string nama;
     vector<dokter*> daftar_dokter;
 
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
     
